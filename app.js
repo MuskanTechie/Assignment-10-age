60572d192cd1ec81206bff44c60e3e7dacbf0bec
MuskanTechie <syedamuskanfatima512@gmail.com>
2025-04-27 23:44:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// Average age
+let std1= parseFloat(prompt("Enter marks of 1st student:"));
+let std2= parseFloat(prompt("Enter marks of 2nd student:"));
+let std3= parseFloat(prompt("Enter marks of 3rd student:"));
+let std4= parseFloat(prompt("Enter marks of 4th student:"));
+let std5= parseFloat(prompt("Enter marks of 5th student:"));
+let std6= parseFloat(prompt("Enter marks of 6th student:"));
+let std7= parseFloat(prompt("Enter marks of 7th student:"));
+let std8= parseFloat(prompt("Enter marks of 8th student:"));
+let std9= parseFloat(prompt("Enter marks of 9th student:"));
+let std10= parseFloat(prompt("Enter marks of 10th student:"));
+let sum =std1+std2+std3+std4+std5+std6+std7+std8+std9+std10;
+let Average=sum/10
+alert("the Average is:" + Average);
